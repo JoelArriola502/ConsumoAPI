f4fb2d32d6539e28e2c8274bf04dd1bebbb8413c
JoelArriola502 <cdJoelArriola502@gmail.com>
2023-09-07 14:04:46 -0600

    segundo commit mas personalisacion

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,32 +1,125 @@
-const marvel={
-    render:()=>{
-        //191c006ab6e8a64f7213fe61f0006a51336172e79acaa2d922db5b86aff95b9b58f99d168
-        const URL_APLI='https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=acaa2d922db5b86aff95b9b58f99d168&hash=2cbbcdacbdf6cd8a191ba89a133d58a2';
-        const Mostrar=document.querySelector('#marvel');
-        let contentHTML='';
-        fetch(URL_APLI)
-        .then(res=>res.json())
-        .then((json)=>{
-            console.log(json,'RES.JSON');
-            for(const comic of json.data.results){
-                let comicMostra=comic.urls[0].url;
-                if(!comic.description){
-                    comic.description="Lo siento no hay descripcion";
-                }
-                contentHTML+=` 
-                <div class="col-md-4">
-                <a href="${comicMostra}" class="navbar">
-                <img src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.name}" class="img-thumbnail">
-                </a>
-                <h3 class="title">${comic.name}</h3>
-                <p>${comic.description}</p>
-        
-                </div>
+const URL_APLI='https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=acaa2d922db5b86aff95b9b58f99d168&hash=2cbbcdacbdf6cd8a191ba89a133d58a2';
+const cargar=document.getElementById("marvel");
+
+
+async function HeroesMarvel(){
+fetch(URL_APLI)
+.then(res=>res.json())
+.then(res=>printData(res.data.results))
+.catch(err=>console.log('Se genero un error: ',err));
+const printData=(personaje)=>{
+    console.log(personaje);
+    
+    let html='<div class="Principal">';
+    let i=0;
+    let name=[];
+    let img=[];
+    let descripcion=[];
+    let urls=[];
+    let ides=[];
+    let series=[];
+    let nombre=document.getElementById("id1").value;
+    for(i=0;i<personaje.length;i++){
+        name[i]=personaje[i].name;
+        ides[i]=personaje[i].id;
+        urls[i]=personaje[i].urls[0].url;
+        img[i]=personaje[i];
+        if(name[i]==nombre){
+        descripcion[i]=personaje[i].description;
+        if(!name[i]){
+            name[i]="lo siento no se encontro el nombre del personaje.";
+        }
+        if(!img[i]){
+            img[i]="lo siento no se encontro una imagen del personaje.";
+        }
+        if(!descripcion[i]){
+            descripcion[i]="lo siento no se encontro la descripcion del personaje.";
+        }
+        html=html+`
+        <div class="contenedor">
+        <div class="mostrar">
+        <a href="${urls[i]}" class="direc">
+            <img class="card" src="${img[i].thumbnail.path}.${img[i].thumbnail. extension}" alt="${name[i]}>
+            </a>
+           <div class="datos">
+            <h2>${name[i]}</h2>
+            <h2>${ides[i]}</h2>
+            <p>${descripcion[i]}</p>
             </div>
+          
+        </div>
+    </div>      
+        
         `;
-            }
-            Mostrar.innerHTML=contentHTML;
-        })
+    }else{
+            
+    
     }
+    
+    html=html+'</div>'
+    cargar.innerHTML=html;
+
 }
-marvel.render();
+}
+}
+
+
+
+
+
+async function HeroesTodos(){
+    fetch(URL_APLI)
+    .then(res=>res.json())
+    .then(res=>printData(res.data.results))
+    .catch(err=>console.log('Se genero un error: ',err));
+    const printData=(personaje)=>{
+        console.log(personaje);
+        
+        let html='<div class="Principal">';
+        let i=0;
+        let name=[];
+        let img=[];
+        let descripcion=[];
+        let urls=[];
+        let ides=[];
+        let series=[];
+        let nombre=document.getElementById("id1").value;
+        for(i=0;i<personaje.length;i++){
+            name[i]=personaje[i].name;
+            ides[i]=personaje[i].id;
+            urls[i]=personaje[i].urls[0].url;
+            img[i]=personaje[i];
+            descripcion[i]=personaje[i].description;
+            if(!name[i]){
+                name[i]="lo siento no se encontro el nombre del personaje.";
+            }
+            if(!img[i]){
+                img[i]="lo siento no se encontro una imagen del personaje.";
+            }
+            if(!descripcion[i]){
+                descripcion[i]="lo siento no se encontro la descripcion del personaje.";
+            }
+            html=html+`
+            <div class="contenedor">
+            <span></span>
+            <div class="mostrar">
+            <a href="${urls[i]}" class="direc">
+                <img src="${img[i].thumbnail.path}.${img[i].thumbnail. extension}" alt="${name[i]}">
+                </a>
+                <h2>${name[i]}</h2>
+                <h2>${ides[i]}</h2>
+                <p>${descripcion[i]}</p>
+              
+            </div>
+        </div>      
+            
+            `;
+        }
+        
+        html=html+'</div>'
+        cargar.innerHTML=html;
+    
+    
+    }
+    }
+
